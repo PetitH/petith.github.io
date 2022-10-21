12f4041393de0ae487e76d5e644bfcebf39f3698
PetitH <petit_h@126.com>
2022-10-21 14:55:27 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,14 @@
 module.exports = {
     title: 'laity',
-    themeConfig: {
-        sidebar:[{
-            title: '获取地理位置授权',
-            path: '/uni/map'
-        }],
-        nav: [
-          { text: 'Home', link: '/' },
-          { text: 'uni-app', link: '/uni/' },
-          { text: 'vue', link: '/vue/' },
-        ]
-      }
+    // themeConfig: {
+    //     sidebar:[{
+    //         title: '获取地理位置授权',
+    //         path: '/uni/map'
+    //     }],
+    //     nav: [
+    //       { text: 'Home', link: '/' },
+    //       { text: 'uni-app', link: '/uni/' },
+    //       { text: 'vue', link: '/vue/' },
+    //     ]
+    //   }
 }
