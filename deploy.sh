b344789bdd707cd320289046ec8480c78b0b4502
PetitH <petit_h@126.com>
2022-10-21 15:06:39 +0800

    update dev

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://petith.github.io/
-git push -f git@github.com/PetitH/petith.github.io.git dev
+git push -f git@github.com/PetitH/petith.github.io.git main:dev
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
