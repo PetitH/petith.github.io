b71b4073e53b137856c81483c1bde5a83de1b70a
PetitH <petit_h@126.com>
2022-10-21 13:54:05 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    title: 'laity',
+    dest: 'public',
+    themeConfig: {
+        sidebar:[{
+            title: '获取地理位置授权',
+            path: '/uni/map'
+        }],
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'uni-app', link: '/uni/' },
+          { text: 'vue', link: '/vue/' },
+        ]
+      }
+}
---
+++ docs/README.md
@@ -0,0 +1,4 @@
+---
+home: true
+tagline: Life is painting a picture, not doing a sum.
+---
